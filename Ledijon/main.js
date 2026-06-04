16f8b1055dc2a270a71600c88d6619f36dbac5d0
ledijonpllana1 <pllanaledijon7@gmail.com>
2026-06-04 19:09:58 +0200

    Ledijon

--- Ledijon/main.js
+++ Ledijon/main.js
@@ -1,3 +1,44 @@
-function showMessage() {
-    alert("Thank you for choosing DriveEasy! Booking page coming soon.");
-}
+const form = document.getElementById("contactForm");
+const success = document.getElementById("success");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const surname = document.getElementById("surname").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    const emailPattern =
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (
+        name === "" ||
+        surname === "" ||
+        email === "" ||
+        message === ""
+    ) {
+        error.style.display = "block";
+        error.textContent = "Please fill in all fields.";
+        success.style.display = "none";
+        return;
+    }
+
+    if (!emailPattern.test(email)) {
+        error.style.display = "block";
+        error.textContent = "Please enter a valid email.";
+        success.style.display = "none";
+        return;
+    }
+
+    error.style.display = "none";
+    success.style.display = "block";
+    success.textContent = "Message sent successfully!";
+
+    form.reset();
+
+    setTimeout(() => {
+        success.style.display = "none";
+    }, 3000);
+});
