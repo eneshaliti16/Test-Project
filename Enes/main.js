07042e317b424e5e5a027304935fede5c668723a
eneshaliti16 <halitienes95@gmail.com>
2026-06-09 18:42:14 +0200

    Part 3

--- Enes/main.js
+++ Enes/main.js
@@ -1,4 +1,12 @@
-function exploreCars(){
-    window.location.href="models.html";
-}
+document.addEventListener("DOMContentLoaded", function () {
 
+    const burger = document.getElementById("menu-toggle");
+    const navLinks = document.getElementById("nav-links");
+
+    burger.addEventListener("click", function () {
+
+        navLinks.classList.toggle("active");
+
+    });
+
+});
